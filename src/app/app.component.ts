427e08303d8ae16cd3bfab7d849fe9244334cabe
unknown <milanzec53@gmail.com>
2023-07-14 10:50:40 +0200

    image object populated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  
+images = [
+  {
+    title:'Beach',
+    url :'https://images.unsplash.com/photo-1473186578172-c141e6798cf4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2glMjBjaGFpcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    title:'Bjj',
+    url :'https://images.unsplash.com/photo-1564415315949-7a0c4c73aab4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aml1JTIwaml0c3V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    title:'Flower',
+    url :'https://plus.unsplash.com/premium_photo-1688045625388-aeeae987b650?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zmxvd2VyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    title:'Poker',
+    url :'https://images.unsplash.com/photo-1596451190630-186aff535bf2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9rZXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+  },
+  {
+    title:'Food',
+    url :'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60'
+  }
+]
+
 }
