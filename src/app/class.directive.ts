32e67c6d8f8f5528771918873198c4fdd5a99f6a
unknown <milanzec53@gmail.com>
2023-08-02 22:48:29 +0200

    setting properties at custom directives

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,12 +1,15 @@
-import { Directive ,ElementRef} from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
 
-  constructor(private element : ElementRef) { 
-    console.log('class directive used')
+  constructor(private element: ElementRef) { }
+
+  @Input() set backgroundColor(color: string) {
+    this.element.nativeElement.style.backgroundColor = color
   }
+
 
 }
