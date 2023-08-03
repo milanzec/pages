45533b6f8cfed8feece7c8e99cb57cbaf96e3590
unknown <milanzec53@gmail.com>
2023-08-03 18:03:44 +0200

    context structural directives

--- src/app/times.directive.ts
+++ src/app/times.directive.ts
@@ -8,15 +8,17 @@ export class TimesDirective {
   constructor(
     private viewContainer: ViewContainerRef,
     private templateRef: TemplateRef<any>
-  ){}
- @Input('appTimes') set render(times: number){
-  this.viewContainer.clear
+  ) { }
+  @Input('appTimes') set render(times: number) {
+    this.viewContainer.clear()
 
-  for (let index = 0; index < times; index++) {
-    this.viewContainer.createEmbeddedView(this.templateRef, {})
+    for (let i = 0; i < times; i++) {
+      this.viewContainer.createEmbeddedView(this.templateRef, {
+        index: i
+      })
+
+    }
 
   }
-
-}   
 
 }
